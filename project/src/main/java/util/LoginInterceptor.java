19e4a7be477787fa9a089908ee98de502c719051
huni6711 <huni6711@naver.com>
2025-10-18 20:00:00 +0900

    feat: util 로그인 인터셉터

---
+++ project/src/main/java/util/LoginInterceptor.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import kr.co.project.member.MemberVO;
+
+public class LoginInterceptor implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(HttpServletRequest request, 
+							HttpServletResponse response, 
+							Object handler)
+							throws Exception {
+		HttpSession sess = request.getSession();
+		MemberVO login = (MemberVO)sess.getAttribute("login");
+		if (login == null) {
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<script>");
+			out.print("alert('로그인 후 사용가능합니다.');");
+			out.print("location.href='/member/login.do';");
+			out.print("</script>");
+			out.close();
+			return false; // 못가
+		}
+		return true; // 가던길가
+	}
+}
