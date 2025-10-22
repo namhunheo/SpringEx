657c4706c9b41d1253f9d3226a3e77c535187be4
huni6711 <huni6711@naver.com>
2025-10-22 21:00:00 +0900

    spring ch10 practice_vo및 vo공부

---
+++ test/src/main/java/chapter10/UserVO.java
@@ -0,0 +1,32 @@
+package chapter10;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+	private int userno;
+	private String id;
+	private String password;
+	private int gender;
+	private int age;
+	private String birth;
+	private String profile_org;
+	private String profile_real;
+	private String introduce;
+	private int local;
+	private Timestamp registdate;
+	
+	private String[] hobby;
+	
+	
+	public String getGenderName() {
+		if (this.gender == 1) {
+			return "남자";
+		} else if (this.gender == 2) {
+			return "여자";
+		}
+		return "";
+	}
+}
